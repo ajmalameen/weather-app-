a66aea496ef5c15055f795e8e905135fe932be58
ajmalameen <ajmalameenkm10@gmail.com>
2022-07-02 15:54:07 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  images: {
+    domains: [
+      'images.unsplash.com', 'openweathermap.org'
+    ]
+  }
+}
